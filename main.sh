5f6fc7b3580d298261c4f733cb688fc07761c8de
root <luki201508@gmail.com>
2020-02-11 09:41:49 +0100

    Script terminado

--- main.sh
+++ main.sh
@@ -15,7 +15,7 @@ default_page=UserName
 # Declaracion de variables
 let username
 let group
-let home_directory
+home_directory="/home"
 let uid
 
 # Salir del programa con su respectivo codigo y eliminar cualquier archivo temporal
@@ -28,6 +28,14 @@ exit_program() {
 			exit 1;;
 	esac
 	exit
+}
+
+error_message() {
+	dialog	--clear \
+		--title "[!ERROR¡]" \
+		--backtitle "$BACKTITLE" \
+		--msgbox "$1" 10 30
+
 }
 
 # Menu principal
@@ -43,6 +51,9 @@ main_menu() {
 		Group	"Elija entre los grupos existentes" \
 		Id	"Ponga un id" \
 		Home	"Establezca el directorio Home" \
+		VerFichero "Ver el fichero donde se guardan los usuarios" \
+		VerInfo "Ver la informacion metida por el usuario" \
+		Añadir	"Añadir usuario al sistema" \
 		Salir	"Salir del programa" \
 		2> /tmp/user-management.tmp.$$
 	if [ ! $? -eq 0 ]
@@ -103,23 +114,81 @@ id_input() {
 	if ! [ "$uid" -eq $uid ]
 	then
 		uid=""
-		dialog  --clear \
-			--title "[!ERROR¡]" \
-			--backtitle "$BACKTITLE" \
-			--msgbox "El id debe ser de tipo numberico entero" 10 30
+		error_message "El id debe ser de tipo numerico entero positivo"
+	fi
+	# Si el uid es negativo lanza un error
+	if [ $uid -lt 0 ]
+	then
+		uid=""
+		error_message "El id debe ser positivo"
 	fi
 	# Si el id ya existe lanza un error
 	if [ ! -z $(getent passwd $uid) ]
 	then
 		uid=""
-		dialog	--clear \
-			--title "[!ERROR¡]" \
-			--backtitle "$BACKTITLE" \
-			--msgbox "El id que ha puesto ya existe" 10 30
+		error_message "El id puesto ya existe ponga otro"
 	fi
+	default_page=Id
 }
 
-home_input() {}
+home_input() {
+	if [ ! -z "$username" ] && [ "$home_directory" == "/home" ]
+	then
+		home_directory="$home_directory/$username"
+	fi
+	dialog  --clear \
+		--title "[Directorio Home]" \
+		--backtitle "$BACKTITLE" \
+		--ok-label "Aceptar" \
+		--cancel-label "Cancelar" \
+		--inputbox "Ponga la direccion del Home" 8 20 "$home_directory" \
+		2> /tmp/user-management.tmp.$$
+	home_directory=$(cat /tmp/user-management.tmp.$$)
+	default_page=Home
+}
+
+ver_fichero_usuarios() {
+	dialog  --clear \
+		--title "[/etc/passwd]" \
+		--backtitle "$BACKTITLE" \
+		--exit-label "Atrás" \
+		--textbox /etc/passwd 40 70
+	default_page=VerFichero
+}
+
+add_user() {
+	if [ -z "$username" ] || [ -z "$group" ] || [ -z "$home_directory" ] || [ -z "$uid"]
+	then
+		error_message "Hay campos sin rellenar"
+	else
+		gid=$(getent group $group | awk -F ":" '{print $3}')
+		user_info="$username::$uid:$gid:usuario $username:$home_directory:/bin/bash"
+		dialog  --clear \
+			--title "[Añadir usuario]" \
+			--backtitle "$BACKTITLE" \
+			--yesno "Crear $user_info?" 10 60
+		option_add_user=$?
+		if [ $? -eq 0 ]
+		then
+			echo "$user_info" >> /etc/passwd && mkdir -p $home_directory
+		fi
+	fi
+	default_page=Añadir
+}
+
+ver_info_usuario() {
+	INFO="Nombre de usuario: $username
+	Nombre de grupo: $group
+	UID del usuario: $uid
+	Directorio home: $home_directory"
+	dialog  --clear \
+		--title "[Usuario info]" \
+		--backtitle "$BACKTITLE" \
+		--ok-label "Aceptar" \
+		--cancel-label "Atrás" \
+		--msgbox "$INFO" 10 40
+	default_page=VerInfo
+}
 
 # Programa principal
 # Bucle infinito hasta que cierre
@@ -131,6 +200,9 @@ while true; do
 		Group) group_input ;;
 		Id) id_input ;;
 		Home) home_input ;;
+		VerFichero) ver_fichero_usuarios ;;
+		VerInfo) ver_info_usuario ;;
+		Añadir) add_user ;;
 		Salir) exit_program 1 ;;
 	esac
 done
