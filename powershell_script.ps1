4a8d4384b68c1a0d00c2c4c7a2326893e05959b9
Lucas Galípolo Uriarte <44504937+luki201508@users.noreply.github.com>
2020-02-11 09:02:51 +0100

    añadida la suma

--- powershell_script.ps1
+++ powershell_script.ps1
@@ -2,9 +2,14 @@
 
 [Int32]$num2 = Read-Host -Prompt "Ponga el segundo número (entre el 1 y 5)"
 
+[int32]$sum = $num1 + $num2
+
 for($i=1;$i-lt 6;$i++) {
     [Int32]$n = $num1 * $num2
     Write-Host -NoNewline ", $n"
     $num1 = $num2
     $num2 = $n
+    $sum += $n
 }
+
+Write-Host $sum
