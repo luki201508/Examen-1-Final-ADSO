ce67602a1b7a9f4f4e19a57317881870c8c42155
Lucas Galípolo Uriarte <44504937+luki201508@users.noreply.github.com>
2020-02-11 09:04:03 +0100

    poniendolo mas bonico

--- powershell_script.ps1
+++ powershell_script.ps1
@@ -2,6 +2,10 @@
 
 [Int32]$num2 = Read-Host -Prompt "Ponga el segundo número (entre el 1 y 5)"
 
+Write-Host "La sucesion aritmetico geometrica para los siguientes 6 elementos es;"
+
+Write-Host -NoNewline "$num1, $num2"
+
 if($num1 -gt $num2) {
     $z = $num1
     $num1 = $num2
@@ -18,4 +22,4 @@ for($i=1;$i-lt 6;$i++) {
     $sum += $n
 }
 
-Write-Host $sum
+Write-Host " y su suma total vale $sum" 
