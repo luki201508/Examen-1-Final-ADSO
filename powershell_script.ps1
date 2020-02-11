91d22e75f672b3911871a6df1e9f807760776f13
Lucas Galípolo Uriarte <44504937+luki201508@users.noreply.github.com>
2020-02-11 09:04:58 +0100

    añadida funcion de error

--- powershell_script.ps1
+++ powershell_script.ps1
@@ -1,16 +1,24 @@
+function ErrorNotBetween($number) {
+    if($number -in 1..5 -eq $false) {
+        Write-Error -Message "El numero no se encuentra entre el uno y el cinco" -ErrorAction Stop
+    }
+}
+
 [Int32]$num1 = Read-Host -Prompt "Ponga el primer número (entre el 1 y 5)"
+ErrorNotBetween $num1
 
 [Int32]$num2 = Read-Host -Prompt "Ponga el segundo número (entre el 1 y 5)"
-
-Write-Host "La sucesion aritmetico geometrica para los siguientes 6 elementos es;"
-
-Write-Host -NoNewline "$num1, $num2"
+ErrorNotBetween $num2
 
 if($num1 -gt $num2) {
     $z = $num1
     $num1 = $num2
     $num2 = $z
 }
+
+Write-Host "La sucesion aritmetico geometrica para los siguientes 6 elementos es;"
+
+Write-Host -NoNewline "$num1, $num2"
 
 [int32]$sum = $num1 + $num2
 
@@ -21,5 +29,4 @@ for($i=1;$i-lt 6;$i++) {
     $num2 = $n
     $sum += $n
 }
-
 Write-Host " y su suma total vale $sum" 
